cde46cc0593b9b971756d45cd714db09a65a8b72
popularkoju <webpk144@gmail.com>
2022-01-30 11:05:16 +0545

    black screen issue

--- lib/Screens/VideoPlayer.dart
+++ lib/Screens/VideoPlayer.dart
@@ -17,8 +17,9 @@ class VideoPlayer extends StatefulWidget {
   _VideoPlayerState createState() => _VideoPlayerState();
 }
 
-class ScreenShotIntent extends Intent{}
-class PauseVideoIntent extends Intent{}
+class ScreenShotIntent extends Intent {}
+
+class PauseVideoIntent extends Intent {}
 
 class _VideoPlayerState extends State<VideoPlayer> {
   @override
@@ -27,11 +28,10 @@ class _VideoPlayerState extends State<VideoPlayer> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     //int _counter = 0;
-    Uint8List _imageFile;
+    // Uint8List _imageFile;
 
     String fileName = "${DateTime.now().microsecondsSinceEpoch}${".png"}";
     String path = r'C:\Users\ACER\Desktop\hello';
@@ -39,111 +39,109 @@ class _VideoPlayerState extends State<VideoPlayer> {
     return Scaffold(
       backgroundColor: Color(0xfff4f4f4),
       body: Center(
-        child: Shortcuts(shortcuts: {
-
-          LogicalKeySet(LogicalKeyboardKey.backspace): ScreenShotIntent(),
-          LogicalKeySet(LogicalKeyboardKey.space): PauseVideoIntent(),
-
-        },
-
+        child: Shortcuts(
+          shortcuts: {
+            LogicalKeySet(LogicalKeyboardKey.backspace): ScreenShotIntent(),
+            LogicalKeySet(LogicalKeyboardKey.space): PauseVideoIntent(),
+          },
           child: Actions(
             actions: {
-              PauseVideoIntent: CallbackAction<PauseVideoIntent>(onInvoke: (intent) => widget.player.playOrPause(),),
-              ScreenShotIntent : CallbackAction<ScreenShotIntent>(onInvoke: (intent) {
-                sController.captureAndSave(
-                    path ,//set path where screenshot will be saved
-                    fileName:fileName
-                );
-                print("ss taken");
+              PauseVideoIntent: CallbackAction<PauseVideoIntent>(
+                onInvoke: (intent) => widget.player.playOrPause(),
+              ),
+              ScreenShotIntent: CallbackAction<ScreenShotIntent>(
+                onInvoke: (intent) {
+                  sController
+                      .capture(delay: Duration(milliseconds: 10))
+                      .then((capturedImage) async {
+                    ShowCapturedWidget(
+                        context, capturedImage!, widget.player);
+                  }).catchError((onError) {
+                    print(onError);
+                  });
+
+                  print("ss taken");
                 },
-
-
               )
             },
-
-            child: Screenshot(
-              controller: sController,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        widget.player.playOrPause();
-                      },
-                      child: Video(
-                        player: widget.player,
-                        scale: 1.0,
-                        // default
-                        showControls: false,
+            child: Column(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      widget.player.playOrPause();
+                    },
+                    child: Screenshot(
+                      controller: sController,
+                      child: Container(
+                        color: Colors.white,
+                        child: Video(
+                          player: widget.player,
+                          scale: 1.0,
+                          // default
+                          showControls: false,
+                        ),
                       ),
                     ),
                   ),
-                  Container(
-                    height: 100,
-                    child: Column(
-                      children: [
-                        StreamBuilder<Object>(
-                          stream: widget.player.positionStream,
-                          initialData: PositionState(),
-                          builder: (context, AsyncSnapshot snapshot) {
-                            PositionState progress = snapshot.data;
-                            return Builder(builder: (context) {
-                              return Slider(
-
-                                value: progress.position!.inSeconds + 0.0,
-                                onChanged: (double value) {},
-                                max: progress.duration!.inSeconds + 0.0,
-                              );
-                              // return Center(
-                              //   child: Text(
-                              //     (progress.position).toString().substring(3, 7) +
-                              //         "/" +
-                              //         (progress.duration).toString().substring(3, 7),
-                              //     style: const TextStyle(
-                              //       color: Colors.black,
-                              //       fontSize: 50,
-                              //     ),
-                              //   ),
-                              // );
-                            });
-                          },
-                        ),
-                        Row(
-                          children:[ ElevatedButton(
+                ),
+                Container(
+                  height: 100,
+                  child: Column(
+                    children: [
+                      StreamBuilder<Object>(
+                        stream: widget.player.positionStream,
+                        initialData: PositionState(),
+                        builder: (context, AsyncSnapshot snapshot) {
+                          PositionState progress = snapshot.data;
+                          return Builder(builder: (context) {
+                            return Slider(
+                              value: progress.position!.inSeconds + 0.0,
+                              onChanged: (double value) {},
+                              max: progress.duration!.inSeconds + 0.0,
+                            );
+                            // return Center(
+                            //   child: Text(
+                            //     (progress.position).toString().substring(3, 7) +
+                            //         "/" +
+                            //         (progress.duration).toString().substring(3, 7),
+                            //     style: const TextStyle(
+                            //       color: Colors.black,
+                            //       fontSize: 50,
+                            //     ),
+                            //   ),
+                            // );
+                          });
+                        },
+                      ),
+                      Row(
+                        children: [
+                          ElevatedButton(
                             onPressed: () {
                               widget.player.playOrPause();
                             },
                             child: const Text("play/pause"),
                           ),
-
-                        ElevatedButton(
-                          onPressed: () {
-                            // //widget.player.playOrPause();
-                            // sController.capture().then((Uint8List image) {
-                            //   //Capture Done
-                            //   setState(() {
-                            //     _imageFile = image;
-                            //   });
-                            // }).catchError((onError) {
-                            //   print(onError);
-                            // });
-                           // final directory = (await getApplicationDocumentsDirectory ()).path; //from path_provide package
-
-
-                            sController.captureAndSave(
-                            path ,//set path where screenshot will be saved
-                            fileName:fileName
-                            );
-                          },
-                          child: const Text("Screenshot"),
-                        ),
-  ],)
-
-                      ],
-                    ),
+                          ElevatedButton(
+                            onPressed: () {
+                              widget.player.pause();
+                              sController
+                                  .capture(delay: Duration(milliseconds: 10))
+                                  .then((capturedImage) async {
+                                ShowCapturedWidget(
+                                    context, capturedImage!, widget.player);
+                              }).catchError((onError) {
+                                print(onError);
+                              });
+                            },
+                            child: const Text("Screenshot"),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
@@ -151,3 +149,55 @@ class _VideoPlayerState extends State<VideoPlayer> {
     );
   }
 }
+
+Future<dynamic> ShowCapturedWidget(
+    BuildContext context, Uint8List capturedImage, Player player) {
+  return showDialog(
+    useSafeArea: false,
+    context: context,
+    builder: (context) => Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            child: Expanded(
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        player.play();
+                      },
+                      icon: Icon(Icons.arrow_back)),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Text("Captured Screenshot")
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Center(
+                child: capturedImage != null
+                    ? Image.memory(capturedImage)
+                    : Container()),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+              player.play();
+            },
+            child: const Text("Back"),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+// _saved(File image) async {
+//   // final result = await ImageGallerySaver.save(image.readAsBytesSync());
+//   print("File Saved to Gallery");
+// }
